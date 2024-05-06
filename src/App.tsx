8106d467e5b555c8041bb57d66f6e331f08852ae
Saba Gigauri <saba.gigauri@gmail.com>
2024-05-07 00:00:38 +0400

    header is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Header } from "./common/components/_molecules";
 import { MainLayout } from "./common/components/_organisms";
 import Breadcrumbs from "./common/components/_organisms/Breadcrumbs/Breadcrumbs";
 import Footer from "./common/components/footer/Footer";
@@ -5,6 +6,7 @@ import Footer from "./common/components/footer/Footer";
 const App = () => {
   return (
     <MainLayout>
+      <Header />
       <Breadcrumbs />
         <h1>App</h1>
         <Footer />
