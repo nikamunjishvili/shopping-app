60332f508ab2b569b4bc56946dbae0e7aebb9d4c
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-05-13 14:51:57 +0400

    UI done

--- src/common/components/_atoms/Icon/ProductCart.tsx
+++ src/common/components/_atoms/Icon/ProductCart.tsx
@@ -1,4 +1,4 @@
-const ProductCart = ({stroke}: ProductCartType) => {
+const ProductCart = ({ stroke }: ProductCartType) => {
   return (
     <svg
       width="20"
@@ -6,6 +6,7 @@ const ProductCart = ({stroke}: ProductCartType) => {
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="hover:fill-[#00B207]"
     >
       <path
         d="M6.66667 8.83333H4.16667L2.5 18H17.5L15.8333 8.83333H13.3333M6.66667 8.83333V6.33333C6.66667 4.49239 8.15905 3 10 3V3C11.8409 3 13.3333 4.49238 13.3333 6.33333V8.83333M6.66667 8.83333H13.3333M6.66667 8.83333V11.3333M13.3333 8.83333V11.3333"
@@ -13,6 +14,7 @@ const ProductCart = ({stroke}: ProductCartType) => {
         stroke-width="1.3"
         stroke-linecap="round"
         stroke-linejoin="round"
+        className="hover:stroke-[#fff]"
       />
     </svg>
   );
@@ -21,5 +23,5 @@ const ProductCart = ({stroke}: ProductCartType) => {
 export default ProductCart;
 
 interface ProductCartType {
-    stroke: string;
+  stroke: string;
 }
