e2e98912e1faa956ab48244ad35ca965858ecfa3
vaxo <vaxojaniashvili186@gmail.com>
2024-05-13 13:53:57 +0400

    added-checkout

--- src/common/components/_organisms/Breadcrumbs/Breadcrumbs.tsx
+++ src/common/components/_organisms/Breadcrumbs/Breadcrumbs.tsx
@@ -10,12 +10,21 @@ const Breadcrumbs = () => {
         backgroundImage: `url(${Image})`,
       }}
     >
-      <div className="w-[1250px] flex gap-[10px]">
+      <div className="w-[1250px] flex gap-5 items-center">
         <Link to={"/"}>
-          <HomeIcon width={25} height={25} />
+          <div className="py-3 hover:border-b-2 transition">
+            <HomeIcon width={25} height={25} />
+          </div>
         </Link>
-        <Link to={"/Categories"} className="text-white">
-          products
+        <Link to={"/Categories"}>
+          <p className="text-white py-3 hover:border-b-2 transition">
+            products
+          </p>
+        </Link>
+        <Link to="/Billing">
+          <p className="text-white hover:border-b-2 transition py-3">
+            Checkout
+          </p>
         </Link>
       </div>
     </div>
